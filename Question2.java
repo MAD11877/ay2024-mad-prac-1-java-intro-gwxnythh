7942dadea0919ab98a5360f6b78edcf230b9c4ac
gwxnythh <149985905+gwxnythh@users.noreply.github.com>
2024-04-15 14:12:14 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -10,7 +10,7 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
      
-    Scanner in = new Scanner(System.in);
+    //Scanner in = new Scanner(System.in);
 
     System.out.print("Enter your height in meters: ");
     double height = in.nextDouble();
